b778b053859b66568293112b7a1c6c277635442e
y1555 <yatsok.vokiret17@mail.ru>
2025-02-25 19:22:14 +0300

    update

---
+++ WebApplication1/Chathub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace WebApplication1
+
+{
+    public class Chathub:Hub
+    {
+        public async Task Send(string message)
+        {
+            await this.Clients.All.SendAsync("Receive", message);
+        }
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,12 @@
+using WebApplication1;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<Chathub>("/chat");
+app.Run();
